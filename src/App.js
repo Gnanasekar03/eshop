85a9b4841b604ba8213470ed2997029b568b7086
Gnanasekar03 <138458161+Gnanasekar03@users.noreply.github.com>
2024-11-27 19:21:00 +0530

    added Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import "./App.scss";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+// import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import { Home } from "./pages";
 
 const App = () => {
+
+  
   return (
-    <div>
-      <h1>Eshop</h1>
-    </div>
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from "./Admin.module.scss"
+const Admin = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  )
+}
+
+export default Admin
